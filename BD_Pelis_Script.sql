22796af217af81426e33f3613d14e095cb6f2d38
dafhome <d.fernalvarez@gmail.com>
2024-06-04 20:22:45 +0200

    commit

--- BD_Pelis_Script.sql
+++ BD_Pelis_Script.sql
@@ -9,29 +9,22 @@ SELECT * FROM peliculas.peliculas ORDER BY title ASC;
 
 /* Agrupar por género */
 /*Realmente aqui solo muestra una pelicula de cada genero*/
-
 SELECT * FROM peliculas.peliculas GROUP BY genre;
-
 
 /* Ver solo las pelis con recaudación menor que X */
 SELECT * FROM peliculas.peliculas WHERE revenue < 2000000;
-
 
 /* Ver películas entre un año y otro de producción */
 SELECT * FROM peliculas.peliculas WHERE `year` BETWEEN 1997 AND 2010
 ORDER BY `year` ASC;
 
-
 /* Ver cuantas pelis hay en la lista */
 SELECT COUNT(DISTINCT id) as Total_Peliculas FROM peliculas.peliculas;
-
 
 /* Ver la suma de todas las recaudaciones */
 SELECT SUM(revenue) as Total_Recaudacion FROM peliculas.peliculas;
 
-
 /* Ver la media de las recaudaciones */
 SELECT AVG(revenue) as Media_Recaudacion FROM peliculas.peliculas;
-
 
 /* Acuerdate de renombrar las columnas cuando creas necesario (AS) */
